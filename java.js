10101bcf1d2c832a384926fe663ae2bd3dd7ff72
PVCWIRETUCSONWIRE <ks9860374@gmail.com>
2026-01-27 13:18:06 +0530

    Update java.js
    update

--- java.js
+++ java.js
@@ -1,10 +1,14 @@
 document.getElementById('contactBtn').addEventListener('click', () => {
-    alert('Thank you for reaching out! We will contact you shortly.');
+    const phoneNumber ="+917217664609";
+    alert('Thank you for reaching out! calling: '+917217664609');
+    window.location.href="tel:"+917217664609.replace(^s/g,");
+        });
     document.getElementById('homeLink').addEventListener('click', (e) => {
         // Optional: Prevent the page from jumping if you just want the image
         // e.preventDefault(); 
-    
+        const container = document.getElementById('Home link clicked,container found:',container);
         const container = document.getElementById('imageContainer');
         
     });
+
 });
